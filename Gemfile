8dcac03396bf6ad45e9dcc7c115512684080212e
yuporon <ftb.y.m.kashima.07@gmail.com>
2023-02-25 20:42:31 +0900

    binding.pry の導入とUserスペックを修正

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ gem "image_processing", "~> 1.2"
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ], require: false
+  gem 'pry-rails'
 
   gem 'rspec-rails' # add in chapter 2
   gem 'factory_bot_rails'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  # 姓、名、メール、パスワードがあれば有効な状態であること
   it "is valid with a first name, last name and email, and password" do
     user = User.new(
       first_name: "Aaron",
@@ -11,24 +12,49 @@ RSpec.describe User, type: :model do
     expect(user).to be_valid
   end
 
+  # 名がなければ無効な状態であること
   it "is invalid without a first name" do
-    user = User.new(first_name: nil)
+    # TODO
+    # こう書いた方がいいと思ってる
+    user = User.new(
+      first_name: nil,
+      last_name:  "Sumner",
+      email:      "tester@example.com",
+      password:   "dottle-nouveau-pavilion-tights-furze",
+    )
     user.valid?
     expect(user.errors[:first_name]).to include("can't be blank")
   end
 
+  # 姓がなければ無効な状態であること
   it "is invalid without a last name" do
-    user = User.new(last_name: nil)
+    # TODO
+    # こう書いた方がいいと思ってる
+    user = User.new(
+      first_name: "Aaron",
+      last_name:  nil,
+      email:      "tester@example.com",
+      password:   "dottle-nouveau-pavilion-tights-furze",
+    )
     user.valid?
     expect(user.errors[:last_name]).to include("can't be blank")
   end
 
+  # メールアドレスがなければ無効な状態であること
   it "is invalid without an email address" do
-    user = User.new(email: nil)
+    # TODO
+    # こう書いた方がいいと思ってる
+    user = User.new(
+      first_name: "Aaron",
+      last_name:  "Sumner",
+      email:      nil,
+      password:   "dottle-nouveau-pavilion-tights-furze",
+    )
     user.valid?
     expect(user.errors[:email]).to include("can't be blank")
   end
 
+  # 重複したメールアドレスなら無効な状態であること
   it "is invalid with a duplicate email address" do
     User.create(
       first_name:  "Joe",
@@ -46,6 +72,7 @@ RSpec.describe User, type: :model do
     expect(user.errors[:email]).to include("has already been taken")
   end
 
+  # ユーザーのフルネームを文字列として返すこと
   it "returns a user's full name as a string" do
     user = User.new(
       first_name: "John",
