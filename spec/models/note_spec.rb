e77b6cb46329f5eabab4dee373b120a15e91ed58
yuporon <ftb.y.m.kashima.07@gmail.com>
2023-02-25 20:51:02 +0900

    NoteModelスペックを修正

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
+  # TODO
+  # let でかかないんかな？
+  # あまりインスタンス変数使うのはみない気がする
   before do
     @user = User.create(
       first_name: "Joe",
@@ -14,6 +17,7 @@ RSpec.describe Note, type: :model do
     )
   end
 
+  # メッセージ、ユーザー、プロジェクトがあれば有効な状態であること
   it "is valid with a user, project, and message" do
     note = Note.new(
       message: "This is a sample note.",
@@ -23,12 +27,17 @@ RSpec.describe Note, type: :model do
     expect(note).to be_valid
   end
 
+  # メッセージがなければ無効な状態であること
   it "is invalid without a message" do
     note = Note.new(message: nil)
     note.valid?
     expect(note.errors[:message]).to include("can't be blank")
   end
 
+  # 文字列に一致するメッセージを検索する
+  # TODO
+  # こう書いた方がいいと思ってる
+  # describe "#search" do
   describe "search message for a term" do
     before do
       @note1 = @project.notes.create(
@@ -45,13 +54,17 @@ RSpec.describe Note, type: :model do
       )
     end
 
+    # 一致するデータが見つかるとき
     context "when a match is found" do
+      # 一致するデータが返ってくること
       it "returns notes that match the search term" do
         expect(Note.search("first")).to include(@note1, @note3)
       end
     end
 
+    # 一致するデータが1件も見つからないとき
     context "when no match is found" do
+      # 空のコレクションを返すこと
       it "returns an empty collection" do
         expect(Note.search("message")).to be_empty
       end
